5ddd35b979974115565d39b8bcdfc8c22dabdd46
Sammy Guttman <1449@holbertonschool.com>
2022-03-22 16:20:23 -0500

    fix point 1

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,58 +1,117 @@
 ﻿using System;
-
-///<summary>Base class</summary>
-abstract class Base
+/// <summary>
+/// Base class
+/// </summary>
+public abstract class Base
 {
-  public string name = "";
+    /// <summary>
+    /// Name property
+    /// </summary>
+    /// <value>string value</value>
+    private string name {
+        get;
+        set;
+    }
+    /// <summary>
+    /// Override tostring() method
+    /// </summary>
+    /// <returns>string</returns>
 
-	public override string ToString()
-	{
-		return (this.name + " is a " + this.GetType().ToString());
-	}
+    public override string ToString()
+    {
+        return ($"{this.name} is a {this.GetType()}");
+    }
 }
 
-/// <summary> Interactive interface </summary>
-interface IInteractive
-{
+/// <summary>
+/// Ineterface Interactive
+/// </summary>
+public interface IInteractive {
+    /// <summary>
+    /// Intercat method
+    /// </summary>
     void Interact();
 }
 
-/// <summary> Breakable Interface </summary>
-interface IBreakable
-{
-    int durability { get; set; }
-    void Break();
+/// <summary>
+/// Interface Breakable
+/// </summary>
+public interface IBreakable {
+    /// <summary>
+    /// durability integer
+    /// </summary>
+    /// <value>int value</value>
+    int durability {
+        get;
+        set;
+    }
+    /// <summary>
+    /// break method
+    /// </summary>
+    void Break();   
 }
 
-/// <summary> Collectable Interface </summary>
-interface ICollectable
-{
-    bool isCollected { get; set; }
+/// <summary>
+/// Interface Collectable
+/// </summary>
+public interface ICollectable {
+    /// <summary>
+    /// isCOllected property
+    /// </summary>
+    /// <value>Bool value</value>
+    bool isCollected {
+        get;
+        set;
+    }
+    /// <summary>
+    /// Collect method
+    /// </summary>
     void Collect();
-
 }
 
-/// <summary> Class that inherits from Base class and all interfaces </summary>
-class TestObject : Base, IInteractive, IBreakable, ICollectable
-{
-	public int durability { get; set; }
-	public bool isCollected { get; set; }
+/// <summary>
+/// Test object class
+/// </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable {
 
-    /// <summary> Takes place when the object interacts with something </summary>
-	public void Interact()
-	{
+    /// <summary>
+    /// durability property
+    /// </summary>
+    /// <value>int value</value>
+    public int durability {
+        get;
+        set;
+    }
+    /// <summary>
+    /// isCollected
+    /// </summary>
+    /// <value>bool value</value>
+    public bool isCollected {
+        get;
+        set;
+    }
 
-	}
+    /// <summary>
+    /// Interact inmplementation
+    /// </summary>
+    public void Interact()
+    {
 
-    /// <summary> Takes place when the object brakes. </summary>
-	public void Break()
-	{
+    }
 
-	}
+    /// <summary>
+    /// Break implementation
+    /// </summary>
+    public void Break()
+    {
 
-    /// <summary> Takes place when the object collects something.</summary>
-	public void Collect()
-	{
+    }
 
-	}
+    /// <summary>
+    /// COllect implementation
+    /// </summary>
+    public void Collect()
+    {
+
+    }
 }
